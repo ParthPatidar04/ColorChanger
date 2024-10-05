30b337b9a6e3375752f6d1a7cebd4c917c305bbf
ParthPatidar04 <159428842+ParthPatidar04@users.noreply.github.com>
2024-10-05 05:40:11 +0000

    project completed

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const message = document.getElementById('message')
 button.forEach(function(button){
     button.addEventListener('click',function(e){
         body.style.backgroundColor = e.target.id
-        message = `Background Color changes to ${e.target.id} color`
+        message.innerHTML = `Background Color changes to ${e.target.id} color`
     })
 })
 reset.addEventListener('click',function(e){
     body.style.backgroundColor = 'black'
+    message.innerHTML = `Color gets reset`
 })
